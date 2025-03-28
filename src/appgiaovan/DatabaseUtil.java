55c33e39f9c4894eba9c0aeea289faf66815ac31
tungnhann <23521077@gm.uit.edu.vn>
2025-03-28 13:51:43 +0700

    sửa localhost

--- src/appgiaovan/DatabaseUtil.java
+++ src/appgiaovan/DatabaseUtil.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:oracle:thin:@LAPTOP-LTSU53RG:1521/orcl";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
     private static final String USER = "qldonvigiaovan";
     private static final String PASSWORD = "Admin123";
 
