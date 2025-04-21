544ae7a47c36d33ad2f0cc1edda179125a817550
PhamPhien-gif <toilaphien2302@gmail.com>
2025-04-21 11:14:27 +0700

    ok

--- src/appgiaovan/GUI/LoginFrame.java
+++ src/appgiaovan/GUI/LoginFrame.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Asset;
+package appgiaovan.GUI;
 
+import Asset.RegisterFrame;
 import appgiaovan.MainScreen;
 
 /**
