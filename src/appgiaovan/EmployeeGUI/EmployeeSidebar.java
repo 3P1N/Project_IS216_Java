43507481f52d843aaabbb05233019253805a8163
PhamPhien-gif <toilaphien2302@gmail.com>
2025-05-12 20:26:15 +0700

    Chuyển sang kế thừa

--- src/appgiaovan/EmployeeGUI/EmployeeSidebar.java
+++ src/appgiaovan/EmployeeGUI/EmployeeSidebar.java
@@ -6,21 +6,31 @@ import java.util.List;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
-public class EmployeeSidebar extends JPanel {
+//public class EmployeeSidebar extends JPanel {
+//
+//    public EmployeeSidebar() {
+//        // Khai báo danh sách tiêu đề và icon
+//        List<String> items = Arrays.asList("Trang chủ", "Báo cáo", "Quản lý đơn hàng", "Quản lý gói hàng", "Đăng xuất");
+//        List<String> icons = Arrays.asList("home.png","report.png", "order.png", "package.png", "logout.png");
+//
+//        // Thiết lập layout cho Sidebar
+//        setLayout(new BorderLayout());
+//
+//        // Tạo MenuBar và add vào chính JPanel này
+//        MenuBar menu = new MenuBar(items, icons);
+//        add(menu, BorderLayout.CENTER);
+//
+//        // Optionally set preferred size
+//        setPreferredSize(menu.getPreferredSize());
+//    }
+//}
+public class EmployeeSidebar extends MenuBar {
+
+    private static final List<String> items = Arrays.asList("Trang chủ", "Báo cáo", "Quản lý đơn hàng", "Quản lý gói hàng", "Đăng xuất");
+    private static final List<String> icons = Arrays.asList("home.png", "report.png", "order.png", "package.png", "logout.png");
 
     public EmployeeSidebar() {
-        // Khai báo danh sách tiêu đề và icon
-        List<String> items = Arrays.asList("Trang chủ", "Báo cáo", "Quản lý đơn hàng", "Quản lý gói hàng", "Đăng xuất");
-        List<String> icons = Arrays.asList("home.png","report.png", "order.png", "package.png", "logout.png");
-
-        // Thiết lập layout cho Sidebar
-        setLayout(new BorderLayout());
-
-        // Tạo MenuBar và add vào chính JPanel này
-        MenuBar menu = new MenuBar(items, icons);
-        add(menu, BorderLayout.CENTER);
-
-        // Optionally set preferred size
-        setPreferredSize(menu.getPreferredSize());
+            super(EmployeeSidebar.items, EmployeeSidebar.icons);
     }
+    
 }
