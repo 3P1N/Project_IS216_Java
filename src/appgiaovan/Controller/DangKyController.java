b05a37f4e418eb39efcb32478b69211b2236bacb
tungnhann <23521077@gm.uit.edu.vn>
2025-05-21 11:36:43 +0700

    DangKyController

--- src/appgiaovan/Controller/DangKyController.java
+++ src/appgiaovan/Controller/DangKyController.java
@@ -7,6 +7,7 @@ package appgiaovan.Controller;
 import appgiaovan.DAO.KhachHangDAO;
 import appgiaovan.Entity.KhachHang;
 import appgiaovan.Entity.TaiKhoan;
+import javax.swing.JPasswordField;
 
 /**
  *
@@ -14,7 +15,7 @@ import appgiaovan.Entity.TaiKhoan;
  */
 public class DangKyController {
         private KhachHangDAO khachHangDAO=new KhachHangDAO();
-        public boolean KiemTraDinhDang(KhachHang khachHang, TaiKhoan taiKhoan) {
+        public boolean KiemTraDinhDang(KhachHang khachHang, TaiKhoan taiKhoan,JPasswordField passField,JPasswordField passRepeat ) {
             
         //1. họ tên không được để trống
         if (khachHang.getHoTen() == null || khachHang.getHoTen().trim().isEmpty()) {
@@ -29,7 +30,9 @@ public class DangKyController {
             return false;
         }
         //4. Nhập lại mật khẩu phải trùng với mật khẩu
-        
+        if(passField!=passRepeat){
+            return false;
+        }
         //5. CCCD không được để trống
         if (khachHang.getCCCD() == null || khachHang.getCCCD().trim().isEmpty()) {
             return false;
@@ -42,36 +45,6 @@ public class DangKyController {
         if (khachHang.getSDT() == null || !khachHang.getSDT().matches("0\\d{9}")) {
             return false;
         }
-        //8.
-
-
-        // 2. Tên người gửi/nhận không được null hoặc rỗng
-        if (donHang.getTenNguoiGui() == null || donHang.getTenNguoiGui().trim().isEmpty()) {
-            return false;
-        }
-
-        if (donHang.getTenNguoiNhan() == null || donHang.getTenNguoiNhan().trim().isEmpty()) {
-            return false;
-        }
-
-        // 3. Địa chỉ nhận không được null hoặc rỗng
-        if (donHang.getDiaChiNhan() == null || donHang.getDiaChiNhan().trim().isEmpty()) {
-            return false;
-        }
-
-        // 4. Dịch vụ phải là 'Tiết kiệm', 'Nhanh' hoặc 'Hỏa tốc'
-        String dichVu = donHang.getDichVu();
-        if (!(dichVu.equals("Tiết kiệm") || dichVu.equals("Nhanh") || dichVu.equals("Hỏa tốc"))) {
-            return false;
-        }
-
-        // 5. Loại hàng hóa không được null hoặc rỗng
-        if (donHang.getLoaiHangHoa() == null || donHang.getLoaiHangHoa().trim().isEmpty()) {
-            return false;
-        }
-
-        
-
         // Có thể thêm nhiều kiểm tra hơn nếu cần
         return true; // Nếu qua tất cả kiểm tra
     }
