980202e466f947818f7da0f0297622830cbc64a7
trnthphong <tranthephong1908@gmail.com>
2025-05-24 00:16:34 +0700

    NhanVienKhoDAO

--- src/appgiaovan/DAO/NhanVienKhoDAO.java
+++ src/appgiaovan/DAO/NhanVienKhoDAO.java
@@ -8,6 +8,62 @@ package appgiaovan.DAO;
  *
  * @author pc
  */
+import appgiaovan.ConnectDB.ConnectionUtils;
+import appgiaovan.Entity.NhanVienKho;
+import appgiaovan.Entity.TaiKhoan;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NhanVienKhoDAO {
+    public NhanVienKhoDAO(){
+        
+    }
+    
+    public NhanVienKho layThongTinNVKho(int id) throws SQLException, ClassNotFoundException{
+        String sql = "SELECT * FROM NhanVienKho WHERE ID_NhanVien = ?";
+        try (Connection conn = ConnectionUtils.getMyConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()){
+                if (rs.next()){
+                    NhanVienKho nv = new NhanVienKho();
+                    nv.setID_NguoiDung(rs.getInt("ID_NhanVien"));
+                    nv.setID_TaiKhoan(rs.getInt("ID_TaiKhoan"));
+                    nv.setHoTen(rs.getString("HoTen"));
+                    nv.setSDT(rs.getString("SDT"));
+                    nv.setEmail(rs.getString("Email"));
+                    nv.setCCCD(rs.getString("CCCD"));
+                    nv.setNgaySinh(rs.getDate("NgaySinh"));
+                    nv.setGioiTinh(rs.getString("GioiTinh"));
+                    nv.setDiaChi(rs.getString("DiaChi"));
+                    nv.setID_Kho(rs.getInt("ID_Kho"));
+                    nv.setID_QuanLy(rs.getInt("ID_QuanLy"));
+                    nv.setMucLuong(rs.getDouble("MucLuong"));
+                    return nv;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public boolean kiemTraTonTaiThongTin(NhanVienKho nv) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT COUNT(*) FROM NhanVienKho WHERE CCCD = ? OR Email = ? OR SDT = ?";
+        try (Connection conn = ConnectionUtils.getMyConnection(); 
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, nv.getCCCD());
+            ps.setString(2, nv.getEmail());
+            ps.setString(3, nv.getSDT());
+            try (ResultSet rs = ps.executeQuery()) {
+                rs.next();
+                return rs.getInt(1) > 0;
+            }
+        }
+    }
+    
     
 }
